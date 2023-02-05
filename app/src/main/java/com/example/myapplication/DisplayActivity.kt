d7657a09031c6f1cdac33cf184c071687d23b89b
Justin St-Arnaud <justin.st-arnaud@polymtl.ca>
2023-02-05 09:48:01 -0500

    J: Display activity

---
+++ app/src/main/java/com/example/myapplication/DisplayActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+
+class DisplayActivity: AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.card_fragment)
+        val buttonClick = findViewById<Button>(R.id.button)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val exp = intent.getStringExtra("Expiration")
+        val date = findViewById<EditText>(R.id.editTextDate)
+        date.setText(if (exp != null) exp else "None" )
+
+    }
+
+}
