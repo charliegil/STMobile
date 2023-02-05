317691c54a56fc9588a89af7e61f2b278402a071
Justin St-Arnaud <justin.st-arnaud@polymtl.ca>
2023-02-05 10:13:05 -0500

    J: Display activity

--- app/src/main/java/com/example/myapplication/DisplayActivity.kt
+++ app/src/main/java/com/example/myapplication/DisplayActivity.kt
@@ -19,9 +19,9 @@ class DisplayActivity: AppCompatActivity()  {
             startActivity(intent)
         }
 
-        val exp = intent.getStringExtra("Expiration")
-        val date = findViewById<EditText>(R.id.editTextDate)
-        date.setText(if (exp != null) exp else "None" )
+//        val exp = intent.getStringExtra("Expiration")
+//        val date = findViewById<EditText>(R.id.editTextDate)
+//        date.setText(if (exp != null) exp else "None" )
 
     }
 
